276ec925a17f3ce36be7c3aedab9235d3e8c2c09
SOWMYAYALAVARTHI <yalavarthisowmya98@gmail.com>
2025-04-20 04:45:29 -0400

    Updated working deployment link for Hospital project

--- src/utils/projectsData.js
+++ src/utils/projectsData.js
@@ -76,7 +76,7 @@ export const projectsData = [
     {
         image: "https://raw.githubusercontent.com/Krishna7141/Real_Hospital_Management_System/main/screenshots/main_window.png",
         title: "Real Hospital Management System",
-        live: "https://real-hospital-app.vercel.app/",
+        live: "https://real-hospital-landing-bhxkno5xf.vercel.app",
         src: "https://github.com/Krishna7141/Real_Hospital_Management_System",
         stack: "Python, Tkinter, SQLite"
       }
